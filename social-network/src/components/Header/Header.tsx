a8ef12b1469f9294e82d5ed2dc675160f7247934
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-02-02 02:58:12 +0300

    refactor: change header

--- social-network/src/components/Header/Header.tsx
+++ social-network/src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { Avatar } from '@mui/material'
+import { deepPurple } from '@mui/material/colors'
 import { FC } from 'react'
 
 import styles from './Header.module.scss'
@@ -5,7 +7,17 @@ import styles from './Header.module.scss'
 const Header: FC = () => {
   return (
     <div className={styles.content}>
-      <input type="text" className={styles.searchInput} placeholder='Search for something here...'/>
+      <input
+        type="text"
+        className={styles.searchInput}
+        placeholder="Search for something here..."
+      />
+      <div className={styles.user}>
+        <div className={styles.userName}>Remy Sharp</div>
+        <Avatar
+          sx={{ bgcolor: deepPurple[500], width: 45, height: 45 }}
+        />
+      </div>
     </div>
   )
 }
