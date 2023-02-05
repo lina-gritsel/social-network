a77db49d322cf9870dee5f3a0a70f6dcc7bcb093
JDM <shchuka_pasha@mail.ru>
2023-02-05 14:12:11 +0300

    feat: add avatar and change avatar button to settings page

--- social-network/src/pages/SettingsPage/SettingsPage.tsx
+++ social-network/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Avatar, Button } from '@mui/material'
 
+import { userNews } from '../NewsPage/NewsPageComponents/userNews'
 import SettingsForm from './SettingsForm'
 
 import styles from './SettingsPage.module.scss'
@@ -11,11 +13,14 @@ const SettingsPage: FC = () => {
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>{t('settingsTitle')}</h2>
-      <img
-        src="https://support.discord.com/hc/user_images/81TKxGEqVJruMIz7RCN8JA.jpeg"
-        alt="avatar"
-        className={styles.avatar}
-      />
+      <div className={styles.avatarBlock}>
+        <Avatar
+          alt="Remy Sharp"
+          src={userNews[4].avatarImg}
+          className={styles.profileAvatar}
+        />
+        <Button className={styles.editAvatar}>{t('editAvatar')}</Button>
+      </div>
       <SettingsForm />
     </div>
   )
