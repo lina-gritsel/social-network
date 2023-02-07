f826ac876f8c0bea631c5a14cfef6e0f5b459ae1
JDM <shchuka_pasha@mail.ru>
2023-02-07 19:26:37 +0300

    refactor: refactor code

--- social-network/src/pages/RegistrationPage/RegistrationForm/helpers.ts
+++ social-network/src/pages/RegistrationPage/RegistrationForm/helpers.ts
@@ -1,10 +1,13 @@
 import * as yup from 'yup'
 
 export const schema = yup.object({
-  email: yup.string().email('Invalid email').required('Email is a required field'),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .required('Email is a required field'),
   nickname: yup
     .string()
-    .min(2, 'Mininum 1 characters')
+    .min(2, 'Mininum 2 characters')
     .max(15, 'Maximum 15 characters')
     .required('This field is required'),
   password: yup
@@ -13,7 +16,7 @@ export const schema = yup.object({
     .matches(/\d/, 'Password should contain at least one number')
     .matches(
       /(?=.*[a-z])(?=.*[A-Z])\w+/,
-      'Password ahould contain at least one uppercase and lowercase character',
+      'Password should contain at least one uppercase and lowercase character',
     )
     .required('This field is required'),
   date: yup.date().required(),
