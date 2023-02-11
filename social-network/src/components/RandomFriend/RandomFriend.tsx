3390f6bd47397899f3d2c72fe3d0c8f0594d62d4
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-02-11 20:08:23 +0300

    refactor styles news page

--- social-network/src/components/RandomFriend/RandomFriend.tsx
+++ social-network/src/components/RandomFriend/RandomFriend.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import Avatar from '@mui/material/Avatar'
-import { Card, CardHeader } from '@mui/material'
+import { Card } from '@mui/material'
 import FacebookIcon from '@mui/icons-material/Facebook'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import InstagramIcon from '@mui/icons-material/Instagram'
@@ -39,30 +39,31 @@ const Friend: FC<RandomFriend> = ({
   return (
     <Card className={styles.friend}>
       <div className={styles.mightLike}>{t('mightLike')}</div>
-      <div className={styles.cardHeader}>
-        <Avatar
-          sx={{ bgcolor: avatarColor }}
-          aria-label="recipe"
-          alt={name}
-          src={avatarImg}
-          className={styles.avatar}
-        ></Avatar>
-        <div>
-          <div className={styles.title}>{name}</div>
-          <div className={styles.subTitle}>{profession}</div>
+      <div className={styles.wrapperContent}>
+        <div className={styles.cardHeader}>
+          <Avatar
+            sx={{ bgcolor: avatarColor }}
+            aria-label="recipe"
+            alt={name}
+            src={avatarImg}
+            className={styles.avatar}
+          />
+          <div>
+            <div className={styles.title}>{name}</div>
+            <div className={styles.subTitle}>{profession}</div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.icons}>
-        <InstagramIcon />
-        <FacebookIcon />
-        <TwitterIcon />
-      </div>
-      <div className={styles.btnWrapper}>
-        <Button className={styles.ignorFriends} outlined={true}>
-          {t('ignore')}
-        </Button>
-        <Button>{t('follow')}</Button>
+        <div className={styles.icons}>
+          <InstagramIcon />
+          <FacebookIcon />
+          <TwitterIcon />
+        </div>
+        <div className={styles.btnWrapper}>
+          <Button className={styles.ignorFriends} outlined>
+            {t('ignore')}
+          </Button>
+          <Button>{t('follow')}</Button>
+        </div>
       </div>
     </Card>
   )
