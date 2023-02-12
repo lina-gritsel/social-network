d0838617cb1ac950290a814db84c9499a80a6804
JDM <shchuka_pasha@mail.ru>
2023-02-12 14:09:41 +0300

    fix: fix validation on login page

--- social-network/src/pages/LoginPage/LoginForm/helpers.ts
+++ social-network/src/pages/LoginPage/LoginForm/helpers.ts
@@ -2,5 +2,5 @@ import * as yup from 'yup'
 
 export const schema = yup.object({
   name: yup.string().required(),
-  password: yup.string().min(8).required(),
+  password: yup.string().required(),
 })
