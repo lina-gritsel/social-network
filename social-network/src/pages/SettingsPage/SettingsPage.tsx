fff6cc500e6b1e08ca368759e1b066fc3f49c2e8
JDM <shchuka_pasha@mail.ru>
2023-02-14 21:00:35 +0300

    refactor: change setting form styles

--- social-network/src/pages/SettingsPage/SettingsPage.tsx
+++ social-network/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,10 +1,7 @@
 import { FC } from 'react'
-import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
-import { Avatar, Button } from '@mui/material'
 
 import Layout from '../../components/Layout'
-import { getUserInfoSelector } from '../../store/selectors'
 
 import SettingsForm from './SettingsForm'
 
@@ -12,18 +9,10 @@ import styles from './SettingsPage.module.scss'
 
 const SettingsPage: FC = () => {
   const { t } = useTranslation()
-  const userInfo = useSelector(getUserInfoSelector)
-
   return (
     <Layout>
       <div className={styles.wrapper}>
         <h2 className={styles.title}>{t('settingsTitle')}</h2>
-        <div className={styles.avatarBlock}>
-          <Avatar className={styles.profileAvatar}>
-            {userInfo?.name?.charAt(0)}
-          </Avatar>
-          <Button className={styles.editAvatar}>{t('editAvatar')}</Button>
-        </div>
         <SettingsForm />
       </div>
     </Layout>
