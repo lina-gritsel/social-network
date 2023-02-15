43d4c6039737af838d7a50885246cd76b0bb0e0a
JDM <shchuka_pasha@mail.ru>
2023-02-15 23:47:13 +0300

    fix: fix settings form validation

--- social-network/src/pages/SettingsPage/SettingsForm/helpers.ts
+++ social-network/src/pages/SettingsPage/SettingsForm/helpers.ts
@@ -1,8 +1,17 @@
 import * as yup from 'yup'
 
 export const schema = yup.object({
-  email: yup.string().email().required(),
-  name: yup.string().required(),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .max(20)
+    .required('Email is a required field'),
+  name: yup
+    .string()
+    .matches(/^[a-zA-Z ]*$/, 'Nickname must be a string')
+    .min(2, 'Mininum 2 characters')
+    .max(15, 'Maximum 15 characters')
+    .required('This field is required'),
   date: yup.date().required(),
   bio: yup.string(),
   location: yup.string(),
