be0ee1f3ce5399e79c9cfcbfb679d389e9557cc3
Alina <almaksimovic01@gmail.com>
2023-02-27 15:24:08 +0300

    feat: add EmojiModal in CreateComent

--- social-network/src/components/CreateComment/CreateComment.tsx
+++ social-network/src/components/CreateComment/CreateComment.tsx
@@ -1,11 +1,15 @@
 import { FC, KeyboardEvent } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
+import { MoodOutlined } from '@mui/icons-material'
+import Picker from '@emoji-mart/react'
+import data from '@emoji-mart/data'
 
 import sendIcon from '../../assets/icons/send.svg'
-import { getUserInfoSelector } from '../../store/selectors'
+import { getUserInfoSelector, selectTheme } from '../../store/selectors'
 import { pressEnter } from '../../utils'
 import Avatar from '../Avatar'
+import Modal from '../Modal'
 
 import styles from './CreateComment.module.scss'
 
@@ -22,6 +26,7 @@ const CreateComment: FC<CommentProps> = ({
 }) => {
   const { t } = useTranslation()
   const userInfo = useSelector(getUserInfoSelector)
+  const theme = useSelector(selectTheme)
 
   return (
     <div className={styles.wrapper}>
@@ -36,10 +41,25 @@ const CreateComment: FC<CommentProps> = ({
             pressEnter(e, onSubmit)
           }
         />
+        <MoodOutlined className={styles.feeling}/>
         <button className={styles.sendComment} onClick={onSubmit}>
           <img src={sendIcon} />
         </button>
       </div>
+      {/* <Modal
+        open={isEmojiModalVisible}
+        onClose={closeEmojiModal}
+        isDialogActions={false}
+        className={styles.feelingModal}
+        content={
+          <Picker
+            theme={theme}
+            data={data}
+            onEmojiSelect={(e) => onEmojiSelect(e)}
+          />
+        }
+      /> */}
+
     </div>
   )
 }
